20154f2df9a9a56d4f64012ee616f7c5ff4876b1
Ibet <igonzalezviltres@ufl.edu>
2024-10-11 22:56:12 -0400

    Almost done with add Meal

--- frontend/src/components/dashboard.tsx
+++ frontend/src/components/dashboard.tsx
@@ -2,28 +2,33 @@ import {useEffect, useState} from 'react'
 import { Navbar } from "./ui/Navbar";
 import { CircularProgressBar } from "./ui/circular-pogress-bar";
 import { Button } from "./ui/button";
-import { ProgressBar } from "./ui/progress-bar";
+// import { ProgressBar } from "./ui/progress-bar";
 import { MacronutrientProgressBar } from './ui/macronutirents-progressbar';
 // import { AuthApi } from '@/api/AuthApi';
-import { Collapse, CollapseProps} from 'antd';
+// import { Collapse, CollapseProps} from 'antd';
 import { ScrollArea } from './ui/scroll-area';
 import { Separator } from './ui/separator';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 import { Table, TableCell, TableHead, TableHeader, TableRow} from './ui/table';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from './ui/select';
+import { Input } from './ui/form/input';
+import type { InputNumberProps } from 'antd';
+import { InputNumber } from 'antd';
+import { Select as SelectAntd} from 'antd';
 
-const  HistoryProgress = (prop: {date: string, calories: number; totalCalories: number}) => {
-  //Code referenced from https://www.youtube.com/watch?v=PraIL031lno&ab_channel=StudytonightwithAbhishek
-  return (
-    <div className="w-full">
-      <div className="flex justify-between"> 
-        <h3 className="font-bold" >{prop.date}</h3>
-        <span className="font-bold">{prop.calories}/{prop.totalCalories} cals</span>
-      </div>
-      <ProgressBar numerator={prop.calories} denominator={prop.totalCalories}/>
-    </div>
-  )
-};
+// const  HistoryProgress = (prop: {date: string, calories: number; totalCalories: number}) => {
+//   //Code referenced from https://www.youtube.com/watch?v=PraIL031lno&ab_channel=StudytonightwithAbhishek
+//   return (
+//     <div className="w-full">
+//       <div className="flex justify-between"> 
+//         <h3 className="font-bold" >{prop.date}</h3>
+//         <span className="font-bold">{prop.calories}/{prop.totalCalories} cals</span>
+//       </div>
+//       <ProgressBar numerator={prop.calories} denominator={prop.totalCalories}/>
+//     </div>
+//   )
+// };
 
 const MacronutrientSection = (prop: {fat: number; protein: number; carb: number; satFat: number; polFat: number;
                                     monFat: number; traFat: number; sodium: number; potassium: number; fiber:number;
@@ -100,6 +105,15 @@ const MacronutrientSection = (prop: {fat: number; protein: number; carb: number;
 
 
 const MealsSection = () => {
+  const[mealType, setMealType] = useState("");
+  const[mealUnit, setMealUnit] = useState("");
+  const[servingAmount, setServingAmount] = useState(0);
+
+  const handleChangeServingAmount: InputNumberProps['onChange'] = (value) => {
+    if(value != null) {
+      setServingAmount(value);
+    }
+  };
 
   const breakfastData = [
     {food: "Oatmeal", cals: 100},
@@ -127,6 +141,32 @@ const MealsSection = () => {
     {food: "Pie", cals: 300},
     {food: "Chocolate", cals: 190},
   ]
+
+  const foodOptions = [
+    {value: '1' , food: "Apple"},
+    {value: '2', food: "Banana"},
+    {value: '3', food: "Cabbage"}
+  ]
+
+  const SelectSearch = (prop: {value: string, food: string}) => {
+    return (
+      <div >
+        <SelectAntd
+          className="bg-gray-100"
+          showSearch
+          placeholder="Select a Meal"
+          filterOption={(input, option) =>
+            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+          }
+
+          options={[
+            {value: '1', label: "Apple"},
+            {value: '2', label: "Banana"},
+          ]}
+        />
+      </div>
+    )
+  }
 
   const ShowDataInRow = (prop: {food: string; cals: number; fat: number; protein: number; carb: number; satFat: number; polFat: number;
                                 monFat: number; traFat: number; sodium: number; potassium: number; fiber:number;
@@ -304,58 +344,63 @@ const MealsSection = () => {
           <DataDisplay mealType="snack"/>
         </TabsContent>
       </Tabs>
-      <div className="flex justify-center px-4 py-4">
-        <Button className="text-defaultText bg-secondary font-bold text-sm">
-          Add meal
-        </Button>
+      <div className="flex justify-center py-4">
+        <Dialog>
+          <DialogTrigger>
+            <Button className="text-defaultText bg-secondary font-bold text-sm">Add meal</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className='text-center'>Add Meal</DialogTitle>
+            </DialogHeader>
+            <div className="flex justify-center">
+              <div className="space-y-4 justify-center items-center">
+                <div className='flex items-center space-x-[70px]'>
+                  <p>Select Meal</p>
+                  <SelectSearch/>
+                </div>
+                <div  className="flex space-x-8 items-center">
+                  <p>Select Meal Type</p>
+                  <Select onValueChange={(value) => {setMealType(value);}}>
+                    <SelectTrigger className="w-[170px] bg-gray-100">
+                      <SelectValue placeholder='Choose a Meal Type'/>
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value='BREAKFAST'>Breakfast</SelectItem>
+                      <SelectItem value='LUNCH'>Lunch</SelectItem>
+                      <SelectItem value='DINNER'>Dinner</SelectItem>
+                      <SelectItem value='SNACK'>Snack</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+               
+                <div className='flex space-x-[35px] items-center'>
+                    <p>Serving Amount</p>
+                    <InputNumber min={1} max={99} defaultValue={1} onChange={handleChangeServingAmount} className='bg-gray-100 w-[60px]'/>
+                  </div>
+                  <div className='flex space-x-[55px] items-center'>
+                    <p>Serving Units</p>
+                    <Select onValueChange={(value) => {setMealUnit(value);}}>
+                      <SelectTrigger className="w-[170px] bg-gray-100">
+                        <SelectValue placeholder='Choose a Unit'/>
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value='CUP'>Cup</SelectItem>
+                        <SelectItem value='LITRE'>Litre</SelectItem>
+                        <SelectItem value='GRAMS'>Gram</SelectItem>
+                        <SelectItem value='OZ'>Ounce</SelectItem>
+                      </SelectContent>  
+                    </Select>
+                  </div>
+                
+                <div className='w-full flex justify-center'>
+                  <Button className="text-defaultText bg-secondary font-bold text-sm">Add</Button>
+                </div>
+              </div>             
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
-    </div>
-  )
-};
-
-const MoreDataSection =  (prop: {fat: number; protein: number; carb: number; calories: number}) => {
-  const fat = prop.fat;
-  const protein = prop.protein;
-  const carb = prop.carb;
-  const[fatCal, setFatCal] = useState(0.0);
-  const[carbCal, setCarbCal] = useState(0.0);
-  const[proteinCal, setProteinCal] = useState(0.0);
-
-
-  useEffect(() => {
-      //Using https://help.practicebetter.io/hc/en-us/articles/4921636414107-Calculating-Macronutrient-Percentages for the calculations
-
-    setFatCal(fat * 9);
-    setCarbCal(carb * 4);
-    setProteinCal(protein * 4);
-
-  }, [carb, fat, protein])
-
-  const items: CollapseProps['items'] = [
-    {
-      key: '1',
-      label: 'Calories from the Macros',
-      children: 
-        <div className=''>
-          <h4 className='text-md'>Out of <span className='font-bold'> {prop.calories} </span> total Calories:</h4>
-          <div className='indent-4'>
-            <p>Out of <span className='font-bold'>{fatCal} Cals</span> were from Fats</p>
-            <p>Out of <span className='font-bold'>{proteinCal} Cals</span> were from Proteins</p>
-            <p>Out of <span className='font-bold'>{carbCal} Cals</span> were from Carbs</p>
-          </div>
-        </div>
-      ,
-    },
-    {
-      key: '2',
-      label: 'testing',
-      children: <p>"testing"</p>,
-    }
-  ]
-
-  return (
-    <div  className='w-full'>
-      <Collapse accordion items={items} defaultActiveKey={['1']} className='w-full'/>
     </div>
   )
 };
@@ -412,7 +457,7 @@ export const Dashboard = () => {
       */}
       <Navbar />
 
-      <div  className="bg-background h-full w-screen flex:col justify-center items-start py-8 space-x-4 space-y-16">
+      <div  className="bg-background h-screen w-screen flex:col justify-center items-start py-8 space-x-4 space-y-16">
         <div className='flex:col justify-center space-y-4'>
 
           <div className='w-full p-4 bg-secondary'>
@@ -445,10 +490,6 @@ export const Dashboard = () => {
               <MealsSection/>
             </div>
 
-            <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md flex flex-col items-center">
-              <h2 className="text-3xl font-bold text-defaultText text-center">More Data</h2>
-              <MoreDataSection fat={fat} carb={carb} protein={protein} calories={totalCal}/>
-            </div>
           </div>
         </div>
 
