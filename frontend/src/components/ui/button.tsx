7c0050903a6810f1c7f2061e57b88fddeba9b19a
Ibet <igonzalezviltres@ufl.edu>
2024-10-30 12:32:57 -0400

    Added the custom button props back to button.tsx

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -5,59 +5,64 @@ import { Slot } from '@radix-ui/react-slot'
 import { cn } from '@/utils/cn'
 import { Spinner } from '@/components/ui/spinner'
 
-
 /*
-buttonVariants and Button design from shadcn
-https://github.com/shadcn-ui/ui/blob/main/apps/www/registry/default/ui/button.tsx
+  buttonVariants and Button design from shadcn
+  https://github.com/shadcn-ui/ui/blob/main/apps/www/registry/default/ui/button.tsx
 */
 
-const buttonVariants = cva(
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-    {
+export const buttonVariants = cva(
+  'flex gap-x-2 items-center justify-center whitespace-nowrap rounded-lg text-sm font-semibold transition-all duration-150 px-4 py-2',
+  {
       variants: {
-        variant: {
-          default: "bg-primary text-primary-foreground hover:bg-primary/90",
-          destructive:
-            "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-          outline:
-            "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-          secondary:
-            "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-          ghost: "hover:bg-accent hover:text-accent-foreground",
-          link: "text-primary underline-offset-4 hover:underline",
-        },
-        size: {
-          default: "h-10 px-4 py-2",
-          sm: "h-9 rounded-md px-3",
-          lg: "h-11 rounded-md px-8",
-          icon: "h-10 w-10",
-        },
+          variant: {
+              default:
+                  'bg-primary text-primary-foreground shadow hover:opacity-80',
+              destructive:
+                  'bg-destructive text-destructive-foreground hover:bg-destructive/80',
+              outline:
+                  'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+              secondary:
+                  'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+              ghost: 'hover:bg-accent hover:text-accent-foreground',
+              link: 'text-primary underline-offset-4 hover:underline',
+          },
+          size: {
+            default: "h-10 px-4 py-2",
+            sm: "h-9 rounded-md px-3",
+            lg: "h-11 rounded-md px-8",
+            icon: "h-10 w-10",
+          },
       },
       defaultVariants: {
-        variant: "default",
-        size: "default",
+          variant: 'default',
+          size: "default",
       },
-    }
-  )
-  
-  export interface ButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-      VariantProps<typeof buttonVariants> {
-    asChild?: boolean
   }
-  
-  const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, variant, size, asChild = false, ...props }, ref) => {
-      const Comp = asChild ? Slot : "button"
-      return (
-        <Comp
-          className={cn(buttonVariants({ variant, size, className }))}
-          ref={ref}
-          {...props}
-        />
-      )
+)
+
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+    VariantProps<typeof buttonVariants> & {
+        isLoading?: boolean
+        icon?: React.ReactNode
+        asChild?: boolean
     }
-  )
-  Button.displayName = "Button"
-  
-  export { Button, buttonVariants }
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+      { className, variant, isLoading, size, icon, asChild, children, ...props },
+      ref
+  ) => {
+      const Comp = asChild ? Slot : 'button'
+      return (
+          <Comp
+              className={cn(buttonVariants({ variant, size, className }))}
+              ref={ref}
+              {...props}
+          >
+              {isLoading && <Spinner />}
+              {!isLoading && icon && <span>{icon}</span>}
+              {children}
+          </Comp>
+      )
+  }
+)
